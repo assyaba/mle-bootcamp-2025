90379a873e81e4cd58e841ebce68d4963981497d
Assya <assyaba300@gmail.com>
2025-10-14 14:37:58 -0400

    docs: Week 1 README structure (#1)
    * docs: add Week 1 goals and structure to README
    
    * chore: tweak hello.py for Day 2 commit practice
    
    * docs: add day2-notes
    
    * added week 1folrder and hello.py file

---
+++ Hello.py
@@ -0,0 +1,8 @@
+def greet(name: str) -> str:
+    return f"Hellooo, {name}! Welcome to MLE Bootcamp 2025 🚀"
+
+if __name__ == "__main__":
+    print(greet("Assya"))
+    
+# quick tweak for Day 2
+print("Gittt Day 2: practicing commits…")
