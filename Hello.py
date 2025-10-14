d234ed7b5e62cb419c2a2512da353eff68a2d5da
assyaba <assyaba300@gmail.com>
2025-10-14 14:03:51 -0400

    added week 1folrder and hello.py file

--- Hello.py
+++ Hello.py
@@ -1,8 +1,8 @@
 def greet(name: str) -> str:
-    return f"Hello, {name}! Welcome to MLE Bootcamp 2025 🚀"
+    return f"Hellooo, {name}! Welcome to MLE Bootcamp 2025 🚀"
 
 if __name__ == "__main__":
     print(greet("Assya"))
     
 # quick tweak for Day 2
-print("Git Day 2: practicing commits…")
+print("Gittt Day 2: practicing commits…")
